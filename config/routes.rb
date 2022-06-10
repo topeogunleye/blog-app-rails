9c28ec97d2b3e9f0db8d7ba8bfaae9e96f5556d0
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-06-10 07:24:29 +0100

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'blog/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'users#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+
+
+
 end
