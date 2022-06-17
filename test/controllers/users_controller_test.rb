5a8bf32b0a5879502a3bfaa31d98c4957253b227
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-06-17 11:35:43 +0100

    Fix linters

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,12 +1,12 @@
-require "test-helper"
+require 'test-helper'
 
 # UsersControllersTest
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get users_index_url
     assert_response :success
-    assert_template "index"
-    assert_select "h1", "Users"
-    assert_select "p", "There are no users yet."
+    assert_template 'index'
+    assert_select 'h1', 'Users'
+    assert_select 'p', 'There are no users yet.'
   end
 end
