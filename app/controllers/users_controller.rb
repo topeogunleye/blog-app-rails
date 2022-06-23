3e8a172e467f95054362f9b0d0afd6149602a8ab
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-06-23 16:28:35 +0100

    Add changes to controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
-# app/controllers/users_controller.rb
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
 end
