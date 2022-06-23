1d850c2d5e000b7a2de9d5caaf077756326f31d9
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-06-23 16:41:32 +0100

    Add views

--- db/schema.rb
+++ db/schema.rb
@@ -10,54 +10,60 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_18_081719) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_22_083407) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "Like", force: :cascade do |t|
-    t.string "author_id"
-    t.string "post_id"
-    t.string "createdat"
-    t.string "updatedat"
-    t.index ["author_id"], name: "index_Like_on_author_id"
-  end
-
-  create_table "Post", force: :cascade do |t|
-    t.string "author_id"
-    t.string "title"
-    t.string "text"
-    t.string "createdat"
-    t.string "updatedat"
-    t.string "comments_counter"
-    t.string "likes_counter"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_Post_on_author_id"
-  end
-
-  create_table "User", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.string "bio"
-    t.string "updatedat"
-    t.string "createdat"
-    t.string "posts_counter"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_User_on_name"
-    t.index ["posts_counter"], name: "index_User_on_posts_counter"
-  end
-
   create_table "comments", force: :cascade do |t|
-    t.string "author_id"
-    t.string "post_id"
-    t.string "test"
-    t.string "updatedat"
-    t.string "createdat"
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.text "bio"
+    t.integer "posts_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "role"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
